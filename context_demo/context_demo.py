d89dd1964d0f0bac5d3f2d887fd678a60037578f
liwenqiang <a082833@vastsea.com>
2018-12-05 14:17:20 +0800

    常用钩子函数

--- context_demo/context_demo.py
+++ context_demo/context_demo.py
@@ -1,9 +1,8 @@
-from flask import Flask,current_app
+from flask import Flask,current_app,render_template
 
 app = Flask(__name__)
 
 
-aa = '123'
 
 @app.route('/')
 def hello_world():
@@ -12,13 +11,18 @@ def hello_world():
 
 @app.before_first_request
 def before_first_request():
-    print('before_first_request, %s' %aa)
+    print('在第一次请求视图函数之前执行的钩子函数')
 
 
 @app.before_request
 def before_request():
-    print('在试图函数执行前执行的钩子函数')
+    print('在每次请求视图函数执行前执行的钩子函数')
 
+
+@app.errorhandler(404)
+def errorhandler(error):
+    return render_template('404.html'),404
+    print('您请求的页面可能丢在火星了~')
 
 if __name__ == '__main__':
     app.run()
