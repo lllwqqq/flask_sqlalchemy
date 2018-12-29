6706c068e9e559f581dfed994813c4be3941d41b
liwenqiang <a082833@vastsea.com>
2018-12-29 16:43:48 +0800

    自己用Bootstrap写的前端页面，准备更换为官方模板，此为备份自己写的静态页面，后期按需修改

---
+++ zlbbs/static/cms/js/resetpwd.js
@@ -0,0 +1,29 @@
+$(function () {
+    $('#submit').click(function (event) {
+        //阻止按钮默认的提交表单行为
+        event.preventDefault();
+        var oldpwdE = $('input[name=oldpwd]');
+        var newpwdE = $('input[name=newpwd]');
+        var newpwd2E = $('input[name=newpwd2]');
+
+        var oldpwd = oldpwdE.val();
+        var newpwd = newpwdE.val();
+        var newpwd2 = newpwd2E.val();
+
+
+        bbsajax.post({
+            'url': '/cms/resetpwd/',
+            'data': {
+                'oldpwd': oldpwd,
+                'newpwd': newpwd,
+                'newpwd2': newpwd2
+            },
+            'success': function (data) {
+                console.log(data);
+            },
+            'fail': function (error) {
+                console.log(error);
+            }
+        });
+    });
+})
---
+++ zlbbs自写前端版本/config.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+import os
+SECRET_KEY = os.urandom(24)
+
+DEBUG = True
+
+HOSTNAME = '127.0.0.1'
+PORT     = '3306'
+DATABASE = 'zlbbs'
+USERNAME = 'root'
+PASSWORD = 'coursedev'
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
+SQLALCHEMY_DATABASE_URI = DB_URI
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# PERMANENT_SESSION_LIFETIME = '1800'
+CMS_USER_ID = 'SFASDFSAFASDFSAF'
